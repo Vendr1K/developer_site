d2bd1399c4d783eef6ae68d9e9bf79e052bc4211
Pavel Ko <vendrik6@gmail.com>
2023-06-03 19:03:56 +0300

    new content/fix bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <meta  charSet='UTF-8'/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-      <link rel="icon" type='image/svg' sizes='32*32' href="/img/logo.svg" />
+      <link rel="icon" type='./image/svg' sizes='32*32' href="/" />
     </Head>
    
       {/* <Header/> */}
